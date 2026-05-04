2b378e5c7d7d099268d8bccff8beacede5bbc9d4
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-04 15:51:58 +0800

    initial: setup monorepo structure with npm workspaces

---
+++ apps/bot/src/index.ts
@@ -0,0 +1,25 @@
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents:[
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+  if (message.content === "!ping") {
+    message.reply("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
