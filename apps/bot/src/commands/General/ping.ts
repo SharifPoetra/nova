e78d5aa1b6accf787389506418142c87b1009b7c
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-05 18:03:32 +0800

    fix: code format fix

--- apps/bot/src/commands/General/ping.ts
+++ apps/bot/src/commands/General/ping.ts
@@ -17,14 +17,12 @@ export class PingCommand extends Command {
     const startTime = Date.now();
     await interaction.deferReply();
     const endTime = Date.now();
-  
+
     const apiLatency = endTime - startTime;
     const wsLatency = Math.round(this.container.client.ws.ping);
 
     const content =
-      `Pong! 🏓\n` +
-      `**Bot Latency:** ${wsLatency}ms\n` +
-      `**API Latency:** ${apiLatency}ms`;
+      `Pong! 🏓\n` + `**Bot Latency:** ${wsLatency}ms\n` + `**API Latency:** ${apiLatency}ms`;
 
     return interaction.editReply(content);
   }
