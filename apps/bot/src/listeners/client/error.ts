eb9dfabf7387f41ea419ee509e65936a1283a7f7
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-09 14:58:51 +0800

    chore: add error and warn listener and move ready listener to client folder

---
+++ apps/bot/src/listeners/client/error.ts
@@ -0,0 +1,12 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener, Events } from '@sapphire/framework';
+
+@ApplyOptions<Listener.Options>({
+  event: Events.Error,
+  name: 'clientError'
+})
+export class ClientErrorListener extends Listener {
+  public override run(error: Error) {
+    this.container.logger.error('[CLIENT ERROR]', error);
+  }
+}
---
+++ apps/bot/src/listeners/client/warn.ts
@@ -0,0 +1,12 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener } from '@sapphire/framework';
+
+@ApplyOptions<Listener.Options>({
+  event: 'warn',
+  name: 'clientWarn'
+})
+export class ClientWarnListener extends Listener {
+  public override run(message: string) {
+    this.container.logger.warn(`[CLIENT WARN] ${message}`);
+  }
+}
