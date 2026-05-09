4810708a8a9002cf1cf9fd5ca9486d520bf83688
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-09 16:51:40 +0800

    chore: hapus stamina regen interval

--- apps/bot/src/listeners/client/ready.ts
+++ apps/bot/src/listeners/client/ready.ts
@@ -2,7 +2,7 @@ import { Listener } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 
 @ApplyOptions<Listener.Options>({
-  once: true, // Listener ini hanya berjalan sekali (seperti client.once)
+  once: true,
   event: 'clientReady',
 })
 export class ReadyListener extends Listener {
@@ -14,36 +14,9 @@ export class ReadyListener extends Listener {
     this.container.logger.info(`✅ Status: Online`);
     this.container.logger.info(`🤖 User  : ${tag}`);
     this.container.logger.info(`🆔 ID    : ${id}`);
-    this.container.logger.info('--------------------------');
+    this.container.logger.info('----------------------');
 
     // Set activity bot di sini
     this.container.client.user?.setActivity('Economy 2.0 | /help');
-
-    // STAMINA REGEN
-    setInterval(
-      async () => {
-        try {
-          const result = await this.container.db.user.collection.updateMany(
-            { $expr: { $lt: ['$stamina', '$maxStamina'] } },
-            [
-              {
-                $set: {
-                  stamina: {
-                    $min: [{ $add: ['$stamina', 5] }, '$maxStamina'],
-                  },
-                  updatedAt: new Date(),
-                },
-              },
-            ],
-          );
-          if (result.modifiedCount > 0) {
-            this.container.logger.info(`⚡ Regen: ${result.modifiedCount} user +5 stamina`);
-          }
-        } catch (e) {
-          this.container.logger.error('Stamina regen error', e);
-        }
-      },
-      5 * 60 * 1000,
-    );
   }
 }
