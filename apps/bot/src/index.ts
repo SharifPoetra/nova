2934ddaaab48d1d823b7efa8aeeb3b5ceeb2d533
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-14 15:31:55 +0800

    chore: better error handling

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -55,7 +55,7 @@ async function main() {
     await client.login(process.env.DISCORD_TOKEN);
     client.logger.info('🚀 Nova Sapphire sedang meluncur!');
   } catch (error) {
-    client.logger.fatal(error);
+    client.logger.fatal(error instanceof Error ? error.stack || error.message : error);
     process.exit(1);
   }
 }
