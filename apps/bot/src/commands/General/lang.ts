b1a50d2449fc0c4dad79f6a340d00faa627ab28f
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-15 16:07:29 +0800

    refactor(lang): use use applyLocalyzedBuilder and fix some errors

--- apps/bot/src/commands/General/lang.ts
+++ apps/bot/src/commands/General/lang.ts
@@ -1,50 +1,58 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { fetchT } from '@sapphire/plugin-i18next';
+import { applyLocalizedBuilder, fetchT } from '@sapphire/plugin-i18next';
+import { PermissionFlagsBits } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
   name: 'lang',
-  description: 'Ganti bahasa',
+  description: 'Change language',
 })
 export class LangCommand extends Command {
-  public override registerApplicationCommands(r: Command.Registry) {
-    r.registerChatInputCommand((b) =>
-      b
-        .setName(this.name)
-        .setDescription(this.description)
-        .addStringOption((o) =>
-          o
-            .setName('pilih')
-            .setDescription('Bahasa')
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand((builder) =>
+      applyLocalizedBuilder(builder, 'commands/names:lang', 'commands/descriptions:lang')
+        .addStringOption((opt) => {
+          return applyLocalizedBuilder(
+            opt,
+            'commands/lang:option_name',
+            'commands/lang:option_desc',
+          )
             .setRequired(true)
             .addChoices(
-              { name: 'Indonesia', value: 'id' },
-              { name: 'English', value: 'en' },
-              { name: 'Melayu', value: 'ms' },
-              { name: 'Reset', value: 'reset' },
-            ),
-        ),
+              { name: 'Bahasa Indonesia', value: 'id' },
+              { name: 'English', value: 'en-US' },
+            );
+        })
+        .addBooleanOption((opt) => {
+          opt.setName('guild').setNameLocalizations({ id: 'guild', 'en-US': 'guild' });
+          return applyLocalizedBuilder(opt, 'commands/lang:guild_name', 'commands/lang:guild_desc');
+        }),
     );
   }
 
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
     await interaction.deferReply({ ephemeral: true });
-    const choice = interaction.options.getString('pilih', true);
     const t = await fetchT(interaction);
+    const lang = interaction.options.getString('language', true) as 'id' | 'en-US';
+    const isGuild = interaction.options.getBoolean('guild') ?? false;
 
-    if (choice === 'reset') {
+    if (isGuild) {
+      if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
+        return interaction.editReply(t('commands/lang:no_perm'));
+      }
+      await this.container.db.guild.updateOne(
+        { guildId: interaction.guildId! },
+        { $set: { lang } },
+        { upsert: true },
+      );
+    } else {
       await this.container.db.user.updateOne(
         { discordId: interaction.user.id },
-        { $set: { lang: null } },
+        { $set: { lang } },
+        { upsert: true },
       );
-      return interaction.editReply(t('commands/lang:lang_reset'));
     }
 
-    await this.container.db.user.updateOne(
-      { discordId: interaction.user.id },
-      { $set: { lang: choice } },
-      { upsert: true },
-    );
-    return interaction.editReply(t('commands/lang:lang_changed', { lang: choice }));
+    return interaction.editReply(t('commands/lang:lang_changed', { lang }));
   }
 }
