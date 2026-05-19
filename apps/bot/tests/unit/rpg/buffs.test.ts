fd0868c661fba7f88edf87ccd007d4447a386753
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-19 16:23:51 +0800

    style(tests): format buffs.test.ts

--- apps/bot/tests/unit/rpg/buffs.test.ts
+++ apps/bot/tests/unit/rpg/buffs.test.ts
@@ -6,16 +6,18 @@ describe('Buffs', () => {
   it('applyPassiveRegen adds stamina', () => {
     const now = new Date();
     const fiveMinsAgo = new Date(now.getTime() - 5 * 60000);
-    
+
     const user = {
       stamina: 50,
       maxStamina: 100,
       lastPassive: fiveMinsAgo,
-      buffs: [{
-        type: 'stamina_regen',
-        value: 2,
-        expires: new Date(now.getTime() + 60000),
-      }],
+      buffs: [
+        {
+          type: 'stamina_regen',
+          value: 2,
+          expires: new Date(now.getTime() + 60000),
+        },
+      ],
     } as unknown as IUser;
 
     applyPassiveRegen(user);
