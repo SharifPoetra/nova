5893df10416bf879fe77a24247906e66ea654ada
SharifPoetra <sharifpoetra1@gmail.com>
2026-05-22 17:07:24 +0800

    chore: update imports for localized items

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -98,7 +98,7 @@ const client = new SapphireClient({
         'commands/shop',
         'commands/daily',
         'commands/reset',
-        'commands/craft'
+        'commands/craft',
       ],
     },
   },
